17e7545f83adf2e04dd06e2bcaeb282f51870ae2
Nokz <dreamwithnokz@gmail.com>
2021-02-06 23:14:35 +0800

    Add pcmanfm config

--- backup.sh
+++ backup.sh
@@ -5,13 +5,14 @@ dotfiles=(
   ~/.Xresources
   ~/.zshrc
   ~/.i3/
+  ~/.i3status.conf
   ~/.config/compton.conf
   ~/.config/kritarc
   ~/.config/kritashortcutsrc
   ~/.config/rofi
   ~/.config/nvim/init.vim
   ~/.config/termite/config
-  ~/.i3status.conf
+  ~/.config/pcmanfm/default/pcmanfm.conf
 )
 
 # Creates initial folder then clean it if it exists
@@ -30,6 +31,9 @@ mkdir ./.config/nvim -v
 rm -rf ./.config/nvim/* -v
 mkdir ./.config/termite -v
 rm -rf ./.config/termite/* -v
+mkdir ./.config/pcmanfm -v
+rm -rf ./.config/pcmanfm/* -v
+mkdir ./.config/pcmanfm/default -v
 
 mv ./compton.conf ./.config -v
 mv ./kritarc ./.config -v
@@ -39,4 +43,5 @@ mv ./rofi ./.config -v
 # Configs with subdirectories inside config
 mv ./init.vim ./.config/nvim -v
 mv ./config ./.config/termite -v
+mv ./pcmanfm.conf ./.config/pcmanfm/default -v
 
