03aea4fceb364a3697b8abfa2890ac511acd0ac7
Nokz <dreamwithnokz@gmail.com>
2022-11-13 12:28:57 +0800

    Remove deleted dotfiles on backup script|

--- backup.sh
+++ backup.sh
@@ -3,9 +3,7 @@ dotfiles=(
   ~/.profile
   ~/.tmux.conf
   ~/.Xmodmap
-  ~/.Xresources
   ~/.zshrc
-  ~/.alacritty.yml
   ~/.i3/
   ~/.i3status.conf
   ~/.config/compton.conf
@@ -14,7 +12,6 @@ dotfiles=(
   ~/.config/rofi
   ~/.config/nvim/coc-settings.json
   ~/.config/nvim/init.vim
-  ~/.config/termite/config
   ~/.config/pcmanfm/default/pcmanfm.conf
 )
 
@@ -32,8 +29,6 @@ mkdir ./.config -v
 rm -rf ./.config/* -v
 mkdir ./.config/nvim -v
 rm -rf ./.config/nvim/* -v
-mkdir ./.config/termite -v
-rm -rf ./.config/termite/* -v
 mkdir ./.config/pcmanfm -v
 rm -rf ./.config/pcmanfm/* -v
 mkdir ./.config/pcmanfm/default -v
@@ -46,6 +41,5 @@ mv ./rofi ./.config -v
 # Configs with subdirectories inside config
 mv ./init.vim ./.config/nvim -v
 mv ./coc-settings.json ./.config/nvim -v
-mv ./config ./.config/termite -v
 mv ./pcmanfm.conf ./.config/pcmanfm/default -v
 
